2b4d6b3a4e811052a6ebddd0698037a5902c2266
Govind <86374558+Govind1862@users.noreply.github.com>
2024-09-17 01:17:52 +0530

    Fuck

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
+function deletetodo(){
+    console.log("delete attempted")
+}
+
+
+
+
 function addElement(){
     let a = document.getElementById("add");
     let b = document.createElement("div");
     ele = a.value;
     let c = document.getElementById("list");
-    b.innerHTML = ele + "<button>Delete</button>";
+    b.innerHTML = ele + "<button onclick="deletetodo">Delete</button>";
     c.appendChild(b);
 }
