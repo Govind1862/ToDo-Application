0602fb947d8043a50adbd99c57d75dc80f2daed5
Govind <86374558+Govind1862@users.noreply.github.com>
2024-09-16 22:46:01 +0530

    working add function

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-addElement(){
+function addElement(){
     let a = document.getElementById("add");
     let b = document.createElement("div");
     b.innerHTML = a.value;
-    let c = document.querySelector("body");
-    let d = document.appendChild(b);
+    let c = document.getElementById("list");
+    c.appendChild(b);
 }
